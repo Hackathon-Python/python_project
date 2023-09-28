9d2a340a2008f7d8d3d796af363b65946687f088
Valeriia Harmash <valeriaharmash@gmail.com>
2023-09-28 14:23:11 -0400

    deployement

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from seed import populate_movies_from_api
 
 if __name__ == '__main__':
     populate_movies_from_api()
-    service.run(host="0.0.0.0", port=8000)
+    service.run()
+
+    # host="0.0.0.0", port=8000
